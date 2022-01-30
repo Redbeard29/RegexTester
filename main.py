611a0f3d5cc47a61ee9ae4eef52d78e6406bdd27
Redbeard29 <ben@benshermanmusic.com>
2022-01-30 18:01:36 -0500

    creating repo for regex pattern tester

---
+++ main.py
@@ -0,0 +1,3 @@
+#Goal is to write a program that accepts a regex pattern and a corresponding string that represents a barcode, and to determine
+#whether or not the barcode matches the regex. If it does not match, the program will return a list of the characters that 
+#failed the pattern.
